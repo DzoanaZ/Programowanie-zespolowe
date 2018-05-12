0e3170f8610a4796c5417b42cb92d0a72a56afc2
Paweł <32869706+Przeskocz@users.noreply.github.com>
2018-05-12 11:40:33 +0200

    Płynna nawigacja po menu klienta

--- CarRental/src/rental/client/ClientController.java
+++ CarRental/src/rental/client/ClientController.java
@@ -1,8 +1,8 @@
 package rental.client;
 
-import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
+import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
@@ -12,33 +12,138 @@ import rental.AController;
 public class ClientController extends AController {
 
     @FXML
-    Label myPersolanlDataLabel;
+    private Label myPersonalDataLabel;
+    private Label activeLabel;
 
     @FXML
     AnchorPane contentBox;
 
     private PersonalData personalData;
+    @FXML
+    private TitledPane myProfilTab;
+    @FXML
+    private Label myCurrentOrdersLabel;
+    @FXML
+    private Label myRentalHistoryLabel;
+    @FXML
+    private TitledPane availableCarsTab;
+    @FXML
+    private Label rzeszowLabel;
+    @FXML
+    private Label stalowaLabel;
+    @FXML
+    private Label dynowLabel;
+    @FXML
+    private Label jasloLabel;
+    @FXML
+    private TitledPane applyOrderTab;
+    @FXML
+    private Label askForAvailabilityLabel;
 
     public ClientController() {
         //TODO: log4j implemented
         System.out.println("---ClientController opened");
     }
-
+    
     @FXML
-    public void myProfilTab_Clicked(MouseEvent mouseEvent) {
-        System.out.println("Login: " + login);
-
-        personalData = new PersonalData();
-        contentBox.getChildren().setAll(personalData);
-        personalData.setEmailField(login);
-
-        selectMenuItem(myPersolanlDataLabel);
+    public void menuItem_Clicked(MouseEvent mouseEvent){
+        Object sender = mouseEvent.getSource();
+        Label selectedItem = null;
+        LabelType item = null;
+        
+        if(sender instanceof Label){
+            selectedItem = (Label)sender;
+        }
+        
+        if(selectedItem!=null){
+            if(selectedItem.getId().toString().equals("myPersonalDataLabel"))
+                item = LabelType.myPersonalDataLabel;
+            else
+            if(selectedItem.getId().toString().equals("myCurrentOrdersLabel"))
+                item = LabelType.myCurrentOrdersLabel;
+            else
+            if(selectedItem.getId().toString().equals("myRentalHistoryLabel"))
+                item = LabelType.myRentalHistoryLabel;
+            else
+            if(selectedItem.getId().toString().equals("dynowLabel"))
+                item = LabelType.dynowLabel;
+            else
+            if(selectedItem.getId().toString().equals("jasloLabel"))
+                item = LabelType.jasloLabel;
+            else
+            if(selectedItem.getId().toString().equals("rzeszowLabel"))
+                item = LabelType.rzeszowLabel;
+            else
+            if(selectedItem.getId().toString().equals("stalowaLabel"))
+                item = LabelType.stalowaLabel;
+            else
+            if(selectedItem.getId().toString().equals("askForAvailabilityLabel"))
+                item = LabelType.askForAvailabilityLabel; 
+        }
+        if(item!=null)
+            readPuzzel(item);
+            
     }
 
     private void selectMenuItem(Label label) {
-        label.underlineProperty().set(true);
-        label.setFont(Font.font("System", FontWeight.BOLD, 14));
+        //activeLabel.getStyleClass().
+        if(activeLabel!=null)
+            activeLabel.getStyleClass().setAll("menuItem");
+        
+        activeLabel = label;
+        activeLabel.getStyleClass().add("active");
+    }
+    
+    private void readPuzzel(LabelType item){
+        if(item == null)
+            return;
+        switch(item){
+            case myPersonalDataLabel:
+                personalData = new PersonalData();
+                contentBox.getChildren().setAll(personalData);
+                personalData.setEmailField(login);
+                selectMenuItem(this.myPersonalDataLabel);
+                break;
+                
+            case myCurrentOrdersLabel:
+                selectMenuItem(this.myCurrentOrdersLabel);
+                contentBox.getChildren().clear();
+                break;
+            case myRentalHistoryLabel:
+                selectMenuItem(this.myRentalHistoryLabel);
+                contentBox.getChildren().clear();
+                break;
+            case dynowLabel:
+                selectMenuItem(this.dynowLabel);
+                contentBox.getChildren().clear();
+                break;
+            case jasloLabel:
+                selectMenuItem(this.jasloLabel);
+                contentBox.getChildren().clear();
+                break;
+            case rzeszowLabel:
+                selectMenuItem(this.rzeszowLabel);
+                contentBox.getChildren().clear();
+                break;
+            case stalowaLabel:
+                selectMenuItem(this.stalowaLabel);
+                contentBox.getChildren().clear();
+                break;
+            case askForAvailabilityLabel:
+                selectMenuItem(this.askForAvailabilityLabel);
+                contentBox.getChildren().clear();
+                break;
+        }
     }
 
-    
+    enum LabelType{
+        myPersonalDataLabel,
+        myCurrentOrdersLabel,
+        myRentalHistoryLabel,
+        dynowLabel,
+        jasloLabel,
+        rzeszowLabel,
+        stalowaLabel,
+        askForAvailabilityLabel
+    }
 }
